8cbfdb6470062413f658284dd6aa2dfdca5107d5
Chandu Paritala <60039091+ChanduParitala@users.noreply.github.com>
2021-08-31 09:02:03 +0530

    Update EmpMain.java

--- JDBC hands-on/EmpMain.java
+++ JDBC hands-on/EmpMain.java
@@ -10,34 +10,55 @@ import java.util.Scanner;
 public class EmpMain {
 
 	public static void main(String[] args) throws SQLException {
-		
+		boolean flag = true;
 		Scanner in = new Scanner(System.in);
-		System.out.println("empid: ");
-		int empId = in.nextInt();
-		System.out.println("empName: ");
-		String empName = in.next();
-		System.out.println("DOB(yyyy-MM-dd): ");
-		String date = in.next();
-		DateTimeFormatter sdf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		LocalDate dob = LocalDate.parse(date, sdf);
-		System.out.println("salary: ");
-		int salary = in.nextInt();
-		Employee emp = new Employee(empId, empName, dob, salary);
-		
-		System.out.println("adding to database..");
 		EmployeeDAO empDao = new EmployeeDAO();
-		boolean flag = empDao.addEmployee(emp);
-		if(flag)
-			System.out.println("Insertion done!");
-		else
-			System.out.println("Error!!!");
-		
-		System.out.println("returning all tuples from database..");
-		ArrayList<Employee> empList = empDao.viewEmployee();
-		for(Employee i : empList)
-		{
-			System.out.println(i.toString());
-		}
+		do {
+			System.out.println("1. Add employee\n2. View all Employee Data\n3. Exit");
+			int choice = in.nextInt();
+			switch (choice) {
+			case 1: {
+				System.out.println("empid: ");
+				int empId = in.nextInt();
+				System.out.println("empName: ");
+				String empName = in.next();
+				System.out.println("DOB(yyyy-MM-dd): ");
+				String date = in.next();
+				DateTimeFormatter sdf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+				LocalDate dob = LocalDate.parse(date, sdf);
+				System.out.println("salary: ");
+				int salary = in.nextInt();
+				Employee emp = new Employee(empId, empName, dob, salary);
+
+				System.out.println("adding to database..");
+				boolean f = empDao.addEmployee(emp);
+				if (f)
+					System.out.println("Insertion done!");
+				else
+					System.out.println("Error!!!");
+				break;
+			}
+			case 2: {
+				System.out.println("returning all tuples from database..");
+				ArrayList<Employee> empList = empDao.viewEmployee();
+				for (Employee i : empList) {
+					System.out.println(i.toString()+"\n");
+				}
+				break;
+			}
+			case 3:
+			{
+				flag = false;
+				break;
+			}
+			default: {
+				System.out.println("wrong selection, try again");
+				break;
+			}
+			}
+
+		} while (flag);
+
 	}
 
 }
